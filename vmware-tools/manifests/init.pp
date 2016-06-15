37305853a50a76cd6f27de197ecd788626d3e88a
Calvin Hartwell <mail@calvinhartwell.com>
2016-06-15 10:35:41 +0100

    modified module so it does not install vmware tools/open-vm-tools on physical machines

--- vmware-tools/manifests/init.pp
+++ vmware-tools/manifests/init.pp
@@ -1,6 +1,8 @@
 # vmware-tools puppet module
 
 class vmware-tools {
+      if $is_virtual {	
+	# currently only handles RHEL but could handle other distro as well
 	if  $operatingsystem == "RedHat" {
 		case $operatingsystemrelease {
  		   /^7.*/: {
@@ -28,4 +30,20 @@ class vmware-tools {
 		   }
 		}
 	}
+   }
+   else 
+   {
+	# remove open-vm-tools from physical machines
+        package { 'open-vm-tools':
+            ensure => 'purged',
+        }
+
+	# remove vmware tools from physical machines
+	package { 'vmware-tools-esx-kmods'
+	    ensure => 'purged',
+	}-> 
+	package { 'vmware-tools-esx-nox':
+            ensure => purged,
+        }
+   }
 }
